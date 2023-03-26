9557a45625192ddbef06f1492198a5918431930f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-26 02:40:14 +0100

    fix: modify

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import Values from 'values.js';
 
 import Color from 'components/Color';
-import Input from 'components/Input';
 import Button from 'components/Button';
 import Input from 'components/Input';
 
